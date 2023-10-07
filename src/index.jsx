5337af2a546f6acede4b09a6c8affe29e4e2cd4b
Ivan Tsurkan <curkanivan493@gmail.com>
2023-10-07 11:51:20 +0300

    Index to Jsx

---
+++ src/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
+import { App } from './App'
+import { store } from './strore'
+import { createGlobalStyle } from 'styled-components'
+import { ThemeProvider } from './components/Layout/Theme/ThemeProvider'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const Global = createGlobalStyle`
+	body {
+    padding: 0;
+    margin: 0;
+		height: 100%;
+		background-color: ${ props => props.theme.main };
+    color: ${ props => props.theme.textColor };
+	}
+`
+
+root.render(
+  <HashRouter>
+    <Provider store={store}>
+      <ThemeProvider>
+        <Global />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </HashRouter>
+)
